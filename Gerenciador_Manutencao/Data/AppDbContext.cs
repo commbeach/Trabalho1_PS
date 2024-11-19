dd8ee109bb5564c187c93f5abe9941c413edeeb7
LuizZottich <133720825+LuizZottich@users.noreply.github.com>
2024-11-19 14:04:57 -0300

    corrigindo detalhes

--- Gerenciador_Manutencao/Data/AppDbContext.cs
+++ Gerenciador_Manutencao/Data/AppDbContext.cs
@@ -5,11 +5,8 @@ namespace Gerenciador_Manutencao.Data;
 
 public class AppDbContext : DbContext
 {
-    private readonly DbContextOptions<AppDbContext> _options;
-
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        _options = options;
     }
 
     public DbSet<Item> Itens { get; set; }
@@ -17,9 +14,4 @@ public class AppDbContext : DbContext
     public DbSet<Modelo> Modelos { get; set; }
     public DbSet<OrdemServico> OrdemServicos { get; set; }
     public DbSet<Equipamento> Equipamentos { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-    }
 }
