d3eb22d4f8a7345a81b57e13e028575d3d2200dc
LuizZottich <133720825+LuizZottich@users.noreply.github.com>
2024-11-19 14:09:37 -0300

    corrigindo detalhes

--- Gerenciador_Manutencao/Model/DTO/ModeloDTO.cs
+++ Gerenciador_Manutencao/Model/DTO/ModeloDTO.cs
@@ -4,15 +4,9 @@ namespace Gerenciador_Manutencao.Model.DTO;
 
 public class ModeloDTO
 {
-    [Required(ErrorMessage = "O ID é obrigatório")]
-    public int Id { get; set; }
-
     [Required(ErrorMessage = "O tipo é obrigatório")]
     public string Tipo { get; set; }
 
     [Required(ErrorMessage = "A marca é obrigatória")]
     public string Marca { get; set; }
-
-    [Required(ErrorMessage = "A lista de manutenções é obrigatória")]
-    public List<ManutencaoDTO> Manutencoes { get; set; } = new List<ManutencaoDTO>();
 }
