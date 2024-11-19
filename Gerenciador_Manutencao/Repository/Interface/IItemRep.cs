84912eb8d17b9c67fb93d7a315be4f4056db8cd2
Lucas Lopes <144856960+Lucas2L-Pentest@users.noreply.github.com>
2024-11-19 18:36:52 -0300

    Add files via upload

--- Gerenciador_Manutencao/Repository/Interface/IItemRep.cs
+++ Gerenciador_Manutencao/Repository/Interface/IItemRep.cs
@@ -3,7 +3,8 @@ using Controle_Manutencao.Model;
 namespace Controle_Manutencao.Repository;
 
 public interface IItemRep{
+
         Task cadastrarItem();
-        Task excluirEquipamento();
+        Task excluirItem();
 
 }
