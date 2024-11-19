65bbbf550603ae74971927316d4ca2c325a9a830
LuizZottich <133720825+LuizZottich@users.noreply.github.com>
2024-11-19 14:06:50 -0300

    corrigindo detalhes

--- Gerenciador_Manutencao/Model/DTO/EquipamentoDTO.cs
+++ Gerenciador_Manutencao/Model/DTO/EquipamentoDTO.cs
@@ -4,14 +4,8 @@ namespace Gerenciador_Manutencao.Model.DTO;
 
 public class EquipamentoDTO
 {
-    [Required(ErrorMessage = "O ID é obrigatório")]
-    public int Id { get; set; }
-
     [Required(ErrorMessage = "O tipo é obrigatório")]
     public string Tipo { get; set; }
-
-    [Required(ErrorMessage = "O modelo é obrigatório")]
-    public ModeloDTO Modelo { get; set; }
 
     [Required(ErrorMessage = "O horímetro ou odômetro é obrigatório")]
     public int HorimetroOuOdometro { get; set; }
