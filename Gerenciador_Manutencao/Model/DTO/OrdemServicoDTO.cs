63047e4347d1a02e1e05b3245762bfcc9d1894ce
LuizZottich <133720825+LuizZottich@users.noreply.github.com>
2024-11-19 14:11:22 -0300

    corrigindo detalhes

--- Gerenciador_Manutencao/Model/DTO/OrdemServicoDTO.cs
+++ Gerenciador_Manutencao/Model/DTO/OrdemServicoDTO.cs
@@ -4,18 +4,9 @@ namespace Gerenciador_Manutencao.Model.DTO;
 
 public class OrdemServicoDTO
 {
-    [Required(ErrorMessage = "O ID é obrigatório")]
-    public int Id { get; set; }
-
-    [Required(ErrorMessage = "O ID do equipamento é obrigatório")]
-    public int IdEquipamento { get; set; }
-
     [Required(ErrorMessage = "A data de abertura é obrigatória")]
     public DateTime DataAbertura { get; set; }
 
     [Required(ErrorMessage = "A data de finalização é obrigatória")]
     public DateTime DataFinalizacao { get; set; }
-
-    [Required(ErrorMessage = "A manutenção é obrigatória")]
-    public ManutencaoDTO Manutencao { get; set; }
 }
