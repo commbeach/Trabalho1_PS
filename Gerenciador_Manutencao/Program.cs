8166022eebf420eba8c484d7c0a857a6b15ee47c
LuizZottich <133720825+LuizZottich@users.noreply.github.com>
2024-11-20 20:50:26 -0300

    atualizando Program.cs

--- Gerenciador_Manutencao/Program.cs
+++ Gerenciador_Manutencao/Program.cs
@@ -1,6 +1,38 @@
+using Controle_Manutencao.Repository;
+using Gerenciador_Manutencao.Data;
+using Gerenciador_Manutencao.Repository.Implementacao;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseInMemoryDatabase(databaseName: "TestDb"),
+    ServiceLifetime.Scoped
+);
+
+
+// Registro dos repositories
+builder.Services.AddScoped<IEquipamentoRep, EquipamentoRep>();
+builder.Services.AddScoped<IItemRep, ItemRep>();
+builder.Services.AddScoped<IManutencaoRep, ManutencaoRep>();
+builder.Services.AddScoped<IModeloRep, ModeloRep>();
+builder.Services.AddScoped<IOrdemServicoRep, OrdemServicoRep>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
+
