b12171672e5c45fb90afb727d21241f62a934ef7
LuizZottich <133720825+LuizZottich@users.noreply.github.com>
2024-11-21 22:02:30 -0300

    ajuste atributo Equipamento

--- Gerenciador_Manutencao/Model/Entity/Equipamento.cs
+++ Gerenciador_Manutencao/Model/Entity/Equipamento.cs
@@ -6,5 +6,6 @@ public class Equipamento
     public string Tipo { get; set; }
     public Modelo modelo{ get; set; }
     public int HorimetroOuOdometro { get; set; }
+    public int IdModelo { get; set; }
     
 }
