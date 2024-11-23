ee79d481aa2d614631656495a6e29742303e3522
Lucas Lopes <144856960+Lucas2L-Pentest@users.noreply.github.com>
2024-11-22 22:51:18 -0300

    Add files via upload

--- Gerenciador_Manutencao/Repository/Interface/IOrdemServicoRep.cs
+++ Gerenciador_Manutencao/Repository/Interface/IOrdemServicoRep.cs
@@ -2,10 +2,11 @@ using Gerenciador_Manutencao.Model;
 
 namespace Controle_Manutencao.Repository;
 
-public interface IOrdemServicoRep{
-        Task abrirOrdemServico(DateTime data);
-        Task fecharOrdemServico(DateTime data);
-        Task adicionarItem(Item i);
-        Task removerItem(Item i);
-        Task <List<Item>> listarItens();
+public interface IOrdemServicoRep
+{
+    Task abrirOrdemServico(DateTime data);
+    Task fecharOrdemServico(DateTime data);
+    Task adicionarItem(Item item, int quantidade);
+    Task removerItem(Item item);
+    Task<List<(Item Item, int Quantidade)>> listarItens();
 }
