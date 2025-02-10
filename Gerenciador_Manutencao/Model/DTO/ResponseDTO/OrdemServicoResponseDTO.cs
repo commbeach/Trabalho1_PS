3498d0c12487da80dd3be9b0cc0d6a5540680709
Lucas Lopes <144856960+Lucas2L-Pentest@users.noreply.github.com>
2025-02-10 18:26:07 -0300

    Add files via upload

--- Gerenciador_Manutencao/Model/DTO/ResponseDTO/OrdemServicoResponseDTO.cs
+++ Gerenciador_Manutencao/Model/DTO/ResponseDTO/OrdemServicoResponseDTO.cs
@@ -1,8 +1,13 @@
 namespace Gerenciador_Manutencao.Model.DTO;
+
 public class OrdemServicoResponseDTO
 {
      public int Id { get; set; }
-     public string Tipo { get; set; }
-    public string Marca { get; set; }
-    public List<Manutencao> Manutencoes { get; set; } = new List<Manutencao>();
+    public int idEquipamento { get; set; }
+     public string status { get; set; }
+    public DateTime dataAbertura { get; set; }
+    public DateTime dataFinalizacao { get; set; }
+    public int idManutencao { get; set; }
+  
+
 }
