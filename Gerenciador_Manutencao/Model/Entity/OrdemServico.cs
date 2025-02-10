8afd8ae26cceee5e68e225c952a9f9518ed03a89
Lucas Lopes <144856960+Lucas2L-Pentest@users.noreply.github.com>
2025-02-10 18:27:03 -0300

    Add files via upload

--- Gerenciador_Manutencao/Model/Entity/OrdemServico.cs
+++ Gerenciador_Manutencao/Model/Entity/OrdemServico.cs
@@ -8,5 +8,5 @@ public class OrdemServico
     public DateTime dataAbertura { get; set; }
     public DateTime dataFinalizacao { get; set; }
     public int idManutencao { get; set; }
-    public List<Item> Itens { get; set; } = new List<Item>();
+   
 }
